3842d48fe7a631ebd140750f60e6577fce459983
Mark Allred <markjallred@gmail.com>
2017-12-07 20:10:09 -0700

    Added random normal generation

--- bootstrap.py
+++ bootstrap.py
@@ -1,2 +1,9 @@
+import numpy as np
+
 class BootstrapData(VectorDataSet):
-  
+  len = len(VectorDataSet)
+  stdNormVec = np.zeros(len,dtype=float)
+
+  for i in range(len):
+    stdNormVec[i] = np.random.normal(0.0, 1.0, len)
+
