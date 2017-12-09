4d17a360f83f72a92ba0204e9a1dd625f99cfc0f
markaleptic <markjallred@gmail.com>
2017-12-08 17:31:28 -0700

    Added step 1

--- bootstrap.py
+++ bootstrap.py
@@ -1,9 +1,20 @@
 import numpy as np
 
 class BootstrapData(VectorDataSet):
-  len = len(VectorDataSet)
-  stdNormVec = np.zeros(len,dtype=float)
+    len = len(VectorDataSet)
+    stdNormVec = np.zeros(len, dtype=float)
+    bootVec = np.zeros(len, dtype=float)
 
-  for i in range(len):
-    stdNormVec[i] = np.random.normal(0.0, 1.0, len)
+
+
+    while bootVec[len-1] != 0:
+        pos = np.random.randint(0, len, 1)
+        bootVec[i] = VectorDataSet[pos]
+
+        if pos + 1 > len:
+            # stop
+
+        for i in range(len):
+            # keep moving 
+
 
