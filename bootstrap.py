d0fd316c2d0fe4be9bdee8c8f334ffe912054d98
markaleptic <markjallred@gmail.com>
2017-12-09 17:23:16 -0700

    Created commodity class and wrote bootstrap pseudo-code

--- bootstrap.py
+++ bootstrap.py
@@ -1,20 +1,35 @@
 import numpy as np
 
-class BootstrapData(VectorDataSet):
-    len = len(VectorDataSet)
-    stdNormVec = np.zeros(len, dtype=float)
-    bootVec = np.zeros(len, dtype=float)
+def bootstrap_data(VectorDataSet):
+    # Get the vector
+    # Generate empty vector of the same size
+
+    # iterator i = 0
+    # while i < the number of observations in OG Vector
+        # Set  new_vect[i] = some random
 
 
 
-    while bootVec[len-1] != 0:
-        pos = np.random.randint(0, len, 1)
+
+    vec_len = len(VectorDataSet)
+    std_norm_vec = np.zeros(vec_len, dtype=float)
+    bootVec = np.zeros(vec_len, dtype=float)
+
+
+
+    while bootVec[vec_len-1] != 0:
+        pos = np.random.randint(0, vec_len, 1)
         bootVec[i] = VectorDataSet[pos]
 
-        if pos + 1 > len:
+        if pos + 1 > vec_len:
             # stop
 
-        for i in range(len):
-            # keep moving 
+        for i in range(vec_len):
+            # keep moving
 
 
+def main():
+    oil_vec_1 = np.random.randint(0,100,50,float)
+    oil_vec_2 = np.random.randint(0,100,50,float)
+    rand_vec_1 = bootstrap_data(oil_vec_1)
+    rand_vec_2 = bootstrap_data(oil_vec_2)
